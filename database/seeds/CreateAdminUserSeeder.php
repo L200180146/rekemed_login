ee4f232390cf9c1877b08c785fff6728ed2802db
Putri Siwi Utami <putriu49@gmail.com>
2021-09-13 22:54:53 +0700

    first commit

---
+++ database/seeds/CreateAdminUserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class CreateAdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $user = User::create([
+        // 	'name' => 'Putri Siwi Utami',
+        // 	'email' => 'putriu49@gmail.com',
+        // 	'password' => bcrypt('123456')
+        // ]);
+
+        // $role = Role::create(['name' => 'Admin']);
+        $role = Role::find(1);
+
+        $permissions = Permission::pluck('id','id')->all();
+
+        $role->syncPermissions($permissions);
+
+        // $user->assignRole([$role->id]);
+    }
+}
